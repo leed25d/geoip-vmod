fb70b310de83da7ebc350adeacf06172dd135016
Lee <lee@livejournal.com>
2011-06-21 17:35:47 -0700

    add session memory and mutex

--- src/vmod_geoip.c
+++ src/vmod_geoip.c
@@ -6,10 +6,16 @@
 #include "vcc_if.h"
 
 static GeoIP *gi = NULL;
+static pthread_mutex_t gi_mutex;
+static const char *unknownCountry= "Unknown";
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
+    int ret;
+    ret= pthread_mutex_init(&gi_mutex, NULL);
+    assert(ret == 0);
+
     if(!gi) { gi = GeoIP_new(GEOIP_STANDARD); }
     return (0);
 }
@@ -17,10 +23,27 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 const char *
 vmod_country(struct sess *sp, const char *ip)
 {
+    int ret, w, v;
+    const char *country = NULL;
+    char *cp;
+    if (!gi) return unknownCountry;
 
-  const char *country = NULL;
-  if (!gi) return "Unknown";
-  country = GeoIP_country_code_by_addr(gi, ip);
-  return country ? country : "Unknown";
+    ret = pthread_mutex_lock(&gi_mutex);
+    assert(ret == 0);
 
+    country = GeoIP_country_code_by_addr(gi, ip);
+
+    w = WS_Reserve(sp->wrk->ws, 0);  /* Reserve some work space (w= # bytes reserved) */
+    cp = sp->wrk->ws->f;             /* Front of workspace area */
+    *cp= 0;
+    v= snprintf(cp, w, "%s", country ? country : unknownCountry);
+
+    pthread_mutex_unlock(&gi_mutex);
+
+    if (v > w) {
+        /* Insufficient reserved space, reset and leave */
+        WS_Release(sp->wrk->ws, 0);
+        return (unknownCountry);
+    }
+    return(cp);
 }

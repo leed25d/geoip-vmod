67b4f9ea57a60d1103bd1a0b5dec998269c8b599
Lee Doolan - US Operations Engineer <lee@bil1-varn01.prod.livejournal.org>
2011-06-24 21:32:46 +0000

    add a comment which is a quote from the  ISO 3166-1 FAQ describing unused country codes

--- src/vmod_geoip.c
+++ src/vmod_geoip.c
@@ -1,6 +1,5 @@
 /**/
 #include <stdlib.h>
-#include <pthread.h>
 #include <GeoIP.h>
 
 #include "vrt.h"
@@ -8,7 +7,24 @@
 #include "include/vct.h"
 #include "vcc_if.h"
 
-static const char *unknownCountry= "Unknown";
+//  Are there any code elements available which I myself can
+//  assign in my own application of ISO 3166-1?  Yes. There are
+//  series of codes which the ISO 3166/MA will never use in the
+//  updating process of // ISO 3166-1 and which are freely
+//  available for users.
+//
+//  To quote from ISO 3166-1:2006, clause 8.1.3 User-assigned code
+//  elements:
+//  ,----
+//  | If users need code elements to represent country names not
+//  | included in this part of ISO 3166, the series of letters
+//  | AA, QM to QZ, XA to XZ, and ZZ, and the series AAA to AAZ,
+//  | QMA to QZZ, XAA to XZZ, and ZZA to ZZZ respectively and the
+//  | series of numbers 900 to 999 are available. These users
+//  | should inform the ISO 3166/MA of such use.
+//  `----
+
+static const char *unknownCountry= "AA";
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
